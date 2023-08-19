cdcb2f0720165e24c98781033f47a8cc021b70c5
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-08-19 17:54:44 -0300

    feat: real time stamp, Genesis Block, Transaction Pool, Mining Reward, Difficulty Adjustment, Transaction Validation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::hash::{Hash, Hasher};
 use sha2::{Sha256, Digest};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Transaction {
     sender: String,
     receiver: String,
@@ -56,35 +57,73 @@ impl Block {
 struct Blockchain {
     blocks: Vec<Block>,
     difficulty: usize,
+    pending_transactions: Vec<Transaction>,
+    mining_reward: f64,
 }
 
 impl Blockchain {
     fn new(difficulty: usize) -> Self {
+        let genesis_block = Block {
+            index: 0,
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs(),
+            transactions: Vec::new(),
+            previous_hash: String::from("genesis_block_hash"),
+            hash: String::from("genesis_block_hash"),
+            nonce: 0,
+        };
+
         Blockchain {
-            blocks: Vec::new(),
+            blocks: vec![genesis_block],
             difficulty,
+            pending_transactions: Vec::new(),
+            mining_reward: 10.0,
         }
     }
 
-    fn add_block(&mut self, transactions: Vec<Transaction>, previous_hash: String) {
+    fn add_transaction(&mut self, transaction: Transaction) {
+        self.pending_transactions.push(transaction);
+    }
+
+    fn mine_pending_transactions(&mut self, miner_address: &str) {
+        let reward_transaction = Transaction {
+            sender: String::from("system"),
+            receiver: String::from(miner_address),
+            amount: self.mining_reward,
+        };
+        self.pending_transactions.push(reward_transaction);
+
+        let previous_hash = self.blocks.last().unwrap().hash.clone();
+        let new_block = self.create_block(previous_hash);
+        self.blocks.push(new_block);
+
+        self.pending_transactions.clear();
+    }
+
+    fn create_block(&self, previous_hash: String) -> Block {
         let index = self.blocks.len() as u32;
-        let timestamp = 0; // Set the timestamp (will be adjusted later)
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
         let mut block = Block {
             index,
             timestamp,
-            transactions,
+            transactions: self.pending_transactions.clone(),
             previous_hash,
-            hash: String::new(), // Set the hash (will be calculated later)
-            nonce: 0, // Set the initial nonce
+            hash: String::new(),
+            nonce: 0,
         };
 
-        block.pow(self.difficulty); // Execute Proof of Work
+        block.pow(self.difficulty);
         block.print_info();
-        self.blocks.push(block);
+        block
     }
 
     fn is_chain_valid(&self) -> bool {
-        let mut prev_hash = String::from("previous_hash_here");
+        let mut prev_hash = String::from("genesis_block_hash");
 
         for block in &self.blocks {
             if block.previous_hash != prev_hash {
@@ -106,21 +145,21 @@ fn main() {
     let difficulty = 4;
     let mut blockchain = Blockchain::new(difficulty);
 
-    let transactions = vec![Transaction {
+    blockchain.add_transaction(Transaction {
         sender: String::from("Alice"),
         receiver: String::from("Bob"),
         amount: 18.0,
-    }];
-    let previous_hash = String::from("genesis_block_hash"); // Change this to a unique value
-    blockchain.add_block(transactions, previous_hash);
+    });
 
-    let transactions2 = vec![Transaction {
+    blockchain.mine_pending_transactions("miner_address");
+
+    blockchain.add_transaction(Transaction {
         sender: String::from("Bob"),
         receiver: String::from("Charlie"),
         amount: 10.0,
-    }];
-    let previous_hash2 = blockchain.blocks.last().unwrap().hash.clone(); // Get hash from the last block
-    blockchain.add_block(transactions2, previous_hash2);
+    });
+
+    blockchain.mine_pending_transactions("miner_address");
 
     println!("Blockchain: {:?}", blockchain);
     println!("Is Blockchain Valid? {}", blockchain.is_chain_valid());
